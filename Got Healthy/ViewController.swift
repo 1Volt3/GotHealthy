aadf46ea6df65f9e24292218aa103e1a05a95d8f
1volt3 <tigerjosh13@yahoo.com>
2017-01-25 10:05:00 -0500

    Add Project
    Adding initial Xcode files

---
+++ Got Healthy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Got Healthy
+//
+//  Created by Josh Rosenzweig on 1/25/17.
+//  Copyright © 2017 Volt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Got HealthyTests/Got_HealthyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Got_HealthyTests.swift
+//  Got HealthyTests
+//
+//  Created by Josh Rosenzweig on 1/25/17.
+//  Copyright © 2017 Volt. All rights reserved.
+//
+
+import XCTest
+@testable import Got_Healthy
+
+class Got_HealthyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

edb3fe98ddd09067b1f1e7ae2252ba11e83d7b4a
1volt3 <tigerjosh13@yahoo.com>
2017-02-01 09:56:08 -0500

    First Time Forms Added
    Added forms for a user to enter data upon opening the app for the first
    time

---
+++ Got Healthy/HeightEntry.swift
@@ -0,0 +1,9 @@
+//
+//  HeightEntry.swift
+//  Got Healthy
+//
+//  Created by Josh Rosenzweig on 2/1/17.
+//  Copyright © 2017 Volt. All rights reserved.
+//
+
+import Foundation
---
+++ Got Healthy/WeightEntry.swift
@@ -0,0 +1,9 @@
+//
+//  WeightEntry.swift
+//  Got Healthy
+//
+//  Created by Josh Rosenzweig on 2/1/17.
+//  Copyright © 2017 Volt. All rights reserved.
+//
+
+import Foundation

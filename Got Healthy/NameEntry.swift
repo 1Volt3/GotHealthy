d0900911f948170ca2f41a8901cb1d7e194391d2
1volt3 <tigerjosh13@yahoo.com>
2017-02-08 10:13:52 -0500

    Added Gender Page
    Changed intro pages to include a gender choice.

--- Got Healthy/NameEntry.swift
+++ Got Healthy/NameEntry.swift
@@ -16,9 +16,9 @@ class NameEntry: UIViewController {
         
         let overcastBlueColor = UIColor(red: 0, green: 187/255, blue: 204/255, alpha: 1.0)
         let lightGreyColor = UIColor(red: 197/255, green: 205/255, blue: 205/255, alpha: 1.0)
-        let darkGreyColor = UIColor(red: 52/255, green: 1/255, blue: 1/255, alpha: 1.0)
+        let darkGreyColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1.0)
         
-        let firstNameInput = FloatingLabelText(frame: CGRect(x: 90, y: 60, width: 300, height: 45))
+        let firstNameInput = FloatingLabelText(frame: CGRect(x: 90, y: 60, width: 250, height: 45))
         firstNameInput.placeholder = "First"
         firstNameInput.title = "Your First Name"
         firstNameInput.tintColor = overcastBlueColor
@@ -28,7 +28,7 @@ class NameEntry: UIViewController {
         firstNameInput.selectedLineColor = overcastBlueColor
         self.view.addSubview(firstNameInput)
         
-        let lastNameInput = FloatingLabelText(frame: CGRect(x: 90, y: 120, width: 300, height: 45))
+        let lastNameInput = FloatingLabelText(frame: CGRect(x: 90, y: 120, width: 250, height: 45))
         lastNameInput.placeholder = "Last"
         lastNameInput.title = "Your Last Name"
         lastNameInput.tintColor = overcastBlueColor
@@ -36,7 +36,7 @@ class NameEntry: UIViewController {
         lastNameInput.lineColor = lightGreyColor
         lastNameInput.selectedTitleColor = overcastBlueColor
         lastNameInput.selectedLineColor = overcastBlueColor
-        self.view.addSubview(firstNameInput)
+        self.view.addSubview(lastNameInput)
     }
 
 }

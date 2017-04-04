45cbe9daefd7f6686e678dd526156653823ac551
1volt3 <tigerjosh13@yahoo.com>
2017-04-04 13:08:43 -0400

    File Change

--- Got Healthy/AppDelegate.swift
+++ Got Healthy/AppDelegate.swift
@@ -88,13 +88,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         })
     }
     
-    
     func bgtimer(_ timer:Timer!){
         updateLocation()
         print("Position Report: \(pos)")
     }
 
-    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
